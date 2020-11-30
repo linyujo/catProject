578dfac918ce265a7707395ce139762306ddbce4
raine <raine.lin@tapsystem.co>
2020-11-30 16:41:50 +0800

    [Update]Update mongoose version and migrate collections from mlab to mongoDB altis

--- server/mongoose/index.js
+++ server/mongoose/index.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose';
 
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 5000
+});
 
 // Get the default connection
 const connect = mongoose.connection;
